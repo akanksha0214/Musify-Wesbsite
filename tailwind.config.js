e21bbff0af3167ac0be0ed0e4e4ecb2f64f6c6b0
akanksha0214 <98308651+akanksha0214@users.noreply.github.com>
2024-09-12 22:13:47 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+import { DefaultContext } from 'react-icons/lib';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary:"#aa6d6b",
+        brandDark:"#151616"
+      },
+      fontFamily:{
+        poppins:["Poppins","sans-serif"],
+      },
+      container:{
+        center:true,
+        padding:{
+          DefaultContext:"1rem",
+          sm:"2rem",
+          lg:"4rem",
+          xl:"5rem",
+          "2xl":"6rem",
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
